6df6e4520e7626872bc72a3473139119a5997598
ddames <daviddames@gmail.com>
2020-05-21 16:41:49 -0500

    First Commit.

---
+++ src/build/precache-manifest.925bc496e5144c876ae544e3d142c21b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d34b3ba8f14cacea77e1c8ac5c37be5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1846770c3420c7fc5acc",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "2917070927ac5fccbf65",
+    "url": "/static/js/2.889fedb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.889fedb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1846770c3420c7fc5acc",
+    "url": "/static/js/main.f57ce172.chunk.js"
+  },
+  {
+    "revision": "921ac687539ca31687de",
+    "url": "/static/js/runtime-main.7c2b1bab.js"
+  }
+]);
